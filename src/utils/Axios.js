36596c9df4049915bf3070978e5f3c32b0e714af
mahmudur987 <safemahmud987@agmail.com>
2024-05-14 02:19:31 +0600

    update axios.js

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,10 +1,15 @@
 import axios from "axios";
+let url1 = "http://localhost:5000/api";
+let url2 = "https://kubetools-be.vercel.app/api";
+let url3 = "https://kubetools.io/api";
+
+export const siteToken =
+  "f8b6e4857fa015369fe4512a2d05f73fd297f79884936e93a62e27c36df4e1ec771e04359972304d56438e572b93737d34bb8ba05e80b0cd7ef6b80eb9a4dd2b";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: url2,
   headers: {
-    authorization:
-      "f8b6e4857fa015369fe4512a2d05f73fd297f79884936e93a62e27c36df4e1ec771e04359972304d56438e572b93737d34bb8ba05e80b0cd7ef6b80eb9a4dd2b",
+    authorization: siteToken,
   },
 });
 
