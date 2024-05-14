e8d430f37a1cc96fc471bc38a5d6cf27c82ce3ec
mahmudur987 <safemahmud987@agmail.com>
2024-05-14 14:51:40 +0600

    url2

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -7,7 +7,7 @@ export const siteToken =
   "f8b6e4857fa015369fe4512a2d05f73fd297f79884936e93a62e27c36df4e1ec771e04359972304d56438e572b93737d34bb8ba05e80b0cd7ef6b80eb9a4dd2b";
 
 const Axios = axios.create({
-  baseURL: url3,
+  baseURL: url2,
   headers: {
     authorization: siteToken,
   },
